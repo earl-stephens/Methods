bca991ebbf87e7e9279b0918009d26d103ad1ede
earl-stephens <stephensearl@hotmail.com>
2021-12-24 17:30:17 -0700

    Demonstrate method calls

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,12 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Person person1 = new Person();
+		person1.name = "Joe";
+		person1.sayHello();
+		person1.sayMyName("Jack");
+		person1.sayMyName(person1.name);
+		
 	}
 
 }
